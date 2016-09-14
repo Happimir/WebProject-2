ed3f7417990b4fd63c2f16a8681f52f7730bd333
Phuc <phuc_senpai@bitbucket.org>
2016-09-14 18:37:26 -0400

    added css for the way the puzzle looks, and added js to dynamically add the classes to each of the divs inside of puzzlearea

---
+++ fifteen.js
@@ -0,0 +1,26 @@
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+
+window.onload = function () {
+    var name;
+    var i = 1;
+
+//gets all children of puzzlearea   
+var children = document.getElementById("puzzlearea").childNodes;
+
+//iterates through all children
+for(child in children){
+   
+    //if children is a div then we do something to it
+    if(children[child].nodeName=="DIV")
+        {
+            //add a class that corresponds to a css to the current div child that we are on
+            children[child].classList.add('a'+i);  
+            i = i + 1;
+        }
+}
+
+    
+     
+    
+};
